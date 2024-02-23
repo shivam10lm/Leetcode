08b7491323ff45a141af4e1b2fde345e563490cf
Shivam Gupta <51778763+shivam10lm@users.noreply.github.com>
2024-02-23 12:50:48 -0700

    Update

--- Array/Find missing number in an array.java
+++ Array/Find missing number in an array.java
@@ -1,27 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
 
-
-
-import java.util.*;
-
-public class tUf {
-    public static int missingNumber(int []a, int N) {
-
-        int xor1 = 0, xor2 = 0;
-
-        for (int i = 0; i < N - 1; i++) {
-            xor2 = xor2 ^ a[i]; // XOR of array elements
-            xor1 = xor1 ^ (i + 1); //XOR up to [1...N-1]
+        for(int i = 1; i <= n; i++){
+            ans = ans ^ i;
         }
-        xor1 = xor1 ^ N; //XOR up to [1...N]
 
-        return (xor1 ^ xor2); // the missing number
-    }
-
-    public static void main(String args[]) {
-        int N = 5;
-        int a[] = {1, 2, 4, 5};
-
-        int ans = missingNumber(a, N);
-        System.out.println("The missing number is: " + ans);
+        for(int i = 0; i < n; i++){
+            ans = ans ^ nums[i];
+        }
+        return ans;
+        
     }
 }
