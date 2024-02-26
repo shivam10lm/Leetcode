2380ba17ab69a2f280088115c2918b9dea3778b0
Shivam Gupta <51778763+shivam10lm@users.noreply.github.com>
2024-02-26 14:10:34 -0700

    Create Best Time to Buy and Sell Stock.java

---
+++ Array/Best Time to Buy and Sell Stock.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int max = 0;
+        int n = prices.length;
+
+        for(int i = 1; i < n; i++){
+            int cost = prices[i] - min;
+            max = Math.max(max, cost);
+            min = Math.min(min, prices[i]);
+        }
+        return max;
+
+    }
+}
