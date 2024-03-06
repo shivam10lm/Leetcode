4a81c22b48f0654a569a262c59a29dc6bd09d4c1
Shivam Gupta <51778763+shivam10lm@users.noreply.github.com>
2024-03-06 13:27:18 -0700

    Update Majority Elements(>N3 times).java

--- Array/Majority Elements(>N3 times).java
+++ Array/Majority Elements(>N3 times).java
@@ -1,59 +1,48 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+       ArrayList<Integer> ans = new ArrayList<>();
+       int n = nums.length;
+       int count1 = 0, count2 = 0;
+       int elm1 = Integer.MIN_VALUE, elm2 = Integer.MIN_VALUE;
 
+       for(int i = 0; i < n; i++){
+           if(count1 == 0 && nums[i] != elm2){
+               count1 = 1;
+               elm1 = nums[i];
+           }
+           else if(count2 == 0 && nums[i] != elm1){
+               count2 = 1;
+               elm2 = nums[i];
+           }
+           else if(elm1 == nums[i]){
+               count1++;
+           }
+           else if(elm2 == nums[i]){
+               count2++;
+           }
+           else{
+               count1--;
+               count2--;
+           }
+       }
+        count1 = 0;
+        count2 = 0;
 
-
-import java.util.*;
-
-public class tUf {
-    public static List<Integer> majorityElement(int []v) {
-        int n = v.length; //size of the array
-
-        int cnt1 = 0, cnt2 = 0; // counts
-        int el1 = Integer.MIN_VALUE; // element 1
-        int el2 = Integer.MIN_VALUE; // element 2
-
-        // applying the Extended Boyer Moore's Voting Algorithm:
-        for (int i = 0; i < n; i++) {
-            if (cnt1 == 0 && el2 != v[i]) {
-                cnt1 = 1;
-                el1 = v[i];
-            } else if (cnt2 == 0 && el1 != v[i]) {
-                cnt2 = 1;
-                el2 = v[i];
-            } else if (v[i] == el1) cnt1++;
-            else if (v[i] == el2) cnt2++;
-            else {
-                cnt1--; cnt2--;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == elm1){
+                count1++;
+            }else if(nums[i] == elm2){
+                count2++;
             }
         }
+    int min = n/3 + 1;
 
-        List<Integer> ls = new ArrayList<>(); // list of answers
-
-        // Manually check if the stored elements in
-        // el1 and el2 are the majority elements:
-        cnt1 = 0; cnt2 = 0;
-        for (int i = 0; i < n; i++) {
-            if (v[i] == el1) cnt1++;
-            if (v[i] == el2) cnt2++;
-        }
-
-        int mini = (int)(n / 3) + 1;
-        if (cnt1 >= mini) ls.add(el1);
-        if (cnt2 >= mini) ls.add(el2);
-
-        // Uncomment the following line
-        // if it is told to sort the answer array:
-        //Collections.sort(ls); //TC --> O(2*log2) ~ O(1);
-
-        return ls;
+    if(count1 >= min){
+        ans.add(elm1);
     }
-
-    public static void main(String args[]) {
-        int[] arr = {11, 33, 33, 11, 33, 11};
-        List<Integer> ans = majorityElement(arr);
-        System.out.print("The majority elements are: ");
-        for (int i = 0; i < ans.size(); i++) {
-            System.out.print(ans.get(i) + " ");
-        }
-        System.out.println();
+    if(count2 >= min){
+        ans.add(elm2);
+    }
+        return ans;
     }
 }
