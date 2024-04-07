8e9431fa6544cad1f287bf0f55d9ecaa09b8dac1
Shivam Gupta <51778763+shivam10lm@users.noreply.github.com>
2024-04-07 16:19:12 -0600

    Update Bubble Sort.java

--- Sorting Algorithms/Bubble Sort.java
+++ Sorting Algorithms/Bubble Sort.java
@@ -7,13 +7,17 @@ public class Solution {
         }
         public static void bubbleSort(int[] arr, int n) {
         //Your code goes here
-
+        int didSwap = 0;
         for(int i = n - 1; i >= 0; i--){
             for(int j = 0; j<= i - 1; j++){
                 if(arr[j] > arr[j+ 1]){
                     swap(arr, j, j+1);
+                    didSwap = 1;
                 }
+                
             }
+            if(didSwap == 0)
+                    break;
         }
     }
 
