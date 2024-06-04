c60a55cc5848c23dd716919a33d9d90c51ea5372
Shivam Gupta <51778763+shivam10lm@users.noreply.github.com>
2024-06-03 22:29:26 -0600

    Update Lower Bound.java

--- Binary Search/Lower Bound.java
+++ Binary Search/Lower Bound.java
@@ -2,7 +2,7 @@ public class Solution {
     public static int lowerBound(int []arr, int n, int x) {
         // Write your code here
         int low = 0, high = n - 1;
-        int ansss = n;
+        int ans = n;
         while(low <= high){
             int mid = (low + high) / 2;
 
