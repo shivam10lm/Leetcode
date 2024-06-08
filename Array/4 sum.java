1cb9a7c715bcfe7dfee91d280c77cc07ebfa886d
Shivam Gupta <51778763+shivam10lm@users.noreply.github.com>
2024-06-07 23:21:20 -0600

    Update 4 sum.java

--- Array/4 sum.java
+++ Array/4 sum.java
@@ -10,7 +10,7 @@ class Solution {
                 if(j > i + 1 && nums[j] == nums[j - 1]) continue;
 
                 int k = j + 1;
-                int l = n - 1;
+                int l = n - 1
                 while(k < l){
                     long sum = nums[i] + nums[j];
                     sum += nums[k];
