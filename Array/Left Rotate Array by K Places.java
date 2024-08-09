22316fcaddaa3e5046b12ff80e51e921811af466
Shivam Gupta <51778763+shivam10lm@users.noreply.github.com>
2024-08-08 22:01:47 -0600

    Create Left Rotate Array by K Places.java

---
+++ Array/Left Rotate Array by K Places.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void reverseArray(int[] nums, int start, int end){
+            while(start < end){
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    public void rotateArray(int[] nums, int k) {
+
+        int n = nums.length;
+        if(n == 0) return;
+        k = k % n;
+        reverseArray(nums, 0, k - 1);
+        reverseArray(nums, k, n - 1);
+        reverseArray(nums, 0, n - 1);
+
+    }
+}
