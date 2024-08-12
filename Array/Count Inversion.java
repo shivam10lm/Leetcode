dedfc3a56bc05b7ceeeacba2f1442342da275f83
Shivam Gupta <51778763+shivam10lm@users.noreply.github.com>
2024-08-11 19:42:39 -0600

    Update Count Inversion.java

--- Array/Count Inversion.java
+++ Array/Count Inversion.java
@@ -1,52 +1,51 @@
-import java.util.*;
+class Solution {
 
-public class Solution {
-
-    public static int merger(long[] arr, int low, int mid, int high){
+    public long merger(int[] a, int low, int mid, int high){
         int left = low, right = mid + 1;
-        int count = 0;
-        ArrayList<Long> temp = new ArrayList<>();
+        long count = 0;
+
+        ArrayList<Integer> temp = new ArrayList<>();
 
         while(left <= mid && right <= high){
-            if(arr[left] <= arr[right]){
-                temp.add(arr[left]);
+            if(a[left] <= a[right]){
+                temp.add(a[left]);
                 left++;
-            }
-            else{
-                temp.add(arr[right]);
+            }else{
+                temp.add(a[right]);
                 count += (mid - left + 1);
                 right++;
             }
         }
-        
+
         while(left <= mid){
-            temp.add(arr[left]);
+            temp.add(a[left]);
             left++;
         }
         while(right <= high){
-            temp.add(arr[right]);
+            temp.add(a[right]);
             right++;
         }
-        
+
         for(int i = low; i <= high; i++){
-            arr[i] = temp.get(i - low);
+            a[i] = temp.get(i - low);
         }
-        
         return count;
     }
 
-    public static int mergeSort(long[] arr, int low, int high){
-        int count = 0;
+    public long mergeSort(int[] a, int low, int high){
+        long count = 0;
         if(low >= high) return count;
-            int mid = (low + high) / 2;
-            count += mergeSort(arr, low, mid);
-            count += mergeSort(arr, mid + 1, high);
-            count += merger(arr, low, mid, high);
-        
+
+        int mid = (low + high)/2;
+        count += mergeSort(a, low, mid);
+        count += mergeSort(a, mid+1, high);
+        count += merger(a, low, mid, high);
+
         return count;
     }
 
-    public static long getInversions(long[] arr, int n) {
-        return mergeSort(arr, 0, n - 1);
+    public long numberOfInversions(int[] nums) {
+        int n = nums.length;
+        return mergeSort(nums, 0, n -1);
     }
 }
